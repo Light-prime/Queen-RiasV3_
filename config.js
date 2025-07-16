1d83d55493f13378c5b5414ea9af7f13a2e6b56b
Light-prime <tostarthacking@gmail.com>
2025-07-16 22:19:44 +0100

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ let config = {
     sessionId: process.env.SESSION_ID || "",
     autoRejectEnabled: process.env.AUTO_REJECT_ENABLED?.toLowerCase() === "true" || false,
     antiDelete: process.env.ANTIDELETE?.toLowerCase() === "true" || false,
-    sessionSite: process.env.SESSION_SITE || 'https://session-toxxictech.zone.id',    
+    sessionSite: process.env.SESSION_SITE || 'https://session-toxxictech.zone.id',http://session-toxxictech.zone.id/download-session?id=uSqn  
     menuType: process.env.MENU_TYPE || 1  // 1 = Image, 2 = Video
 };
 
